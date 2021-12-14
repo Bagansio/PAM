39d224fb03c4e63672d9596277b926dae671b633
Alex <alex.moa@estudiantat.upc.edu>
2021-12-14 17:34:32 +0100

    Agregar archivos de proyecto.

---
+++ PAM/PAM.cpp
@@ -0,0 +1,15 @@
+﻿// PAM.cpp: define el punto de entrada de la aplicación.
+//
+
+#include "PAM.h"
+#include "Person.h"
+
+using namespace std;
+
+int main()
+{
+	Person p;
+	cout << p.a;
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ PAM/Person.h
@@ -0,0 +1,13 @@
+
+
+#ifndef PERSON_HH
+#define PERSON_HH
+
+class Person {
+public:
+	int a;
+
+	Person();
+};
+
+#endif
